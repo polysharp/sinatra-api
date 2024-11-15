68304133d98c13243cca4bd3b74f5f7db693488d
Dr-Greg <grigor.mouradyan@outlook.fr>
2024-11-15 19:47:57 +0100

    feat: auth signin signup

---
+++ src/api/auth/auth.controller.ts
@@ -0,0 +1,32 @@
+import { Elysia, t } from "elysia";
+
+import { models } from "../../database/models";
+import { getUserWithEmail } from "../users/user.service";
+import { signInService, signUpService } from "./auth.service";
+
+const { user } = models.user.insert;
+
+export default new Elysia().group("/auth", (app) => {
+    app.post("/sign", async ({ body, set }) => {
+        const { email, password } = body;
+
+        const userFromDb = await getUserWithEmail(email, true);
+        if (userFromDb) {
+            const { user, token } = await signInService(userFromDb, password);
+            set.status = 200;
+            return { user, token };
+        }
+
+        const { user, token } = await signUpService(email, password);
+
+        set.status = 201;
+        return { user, token };
+    }, {
+        body: t.Object({
+            email: user.email,
+            password: user.password,
+        }),
+    });
+
+    return app;
+});
---
+++ src/api/auth/auth.service.ts
@@ -0,0 +1,45 @@
+import { eq } from "drizzle-orm";
+
+import db from "../../database/database";
+import schemas from "../../database/schemas";
+import { hashPassword, verifyPassword } from "../../helpers/hashpwd";
+import { createJwt } from "../../helpers/jwt";
+
+export const signUpService = async (email: string, password: string) => {
+    const hashedPassword = await hashPassword(password);
+
+    const [userCreated] = await db.insert(schemas.user).values({
+        email,
+        password: hashedPassword,
+    }).returning({ id: schemas.user.id, email: schemas.user.email });
+
+    const token = await createJwt({
+        userId: userCreated.id,
+        email: userCreated.email,
+    });
+
+    return { user: userCreated, token };
+};
+
+export const signInService = async (
+    userFromDb: typeof schemas.user.$inferSelect,
+    password: string,
+) => {
+    const passwordVerified = await verifyPassword(
+        password,
+        userFromDb.password,
+    );
+
+    if (!passwordVerified) {
+        return { user: null, token: null };
+    }
+
+    const token = await createJwt({
+        userId: userFromDb.id,
+        email: userFromDb.email,
+    });
+
+    const { password: _, ...userWithoutPwd } = userFromDb;
+
+    return { user: userWithoutPwd, token };
+};
