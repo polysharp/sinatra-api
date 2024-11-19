718d398e2e90a5bc5c415b54af2438b8a4a77b4c
Dr-Greg <grigor.mouradyan@outlook.fr>
2024-11-19 17:48:37 +0100

    feat: update schemas with new data model

--- src/database/schemas/api-keys.schema.ts
+++ src/database/schemas/api-keys.schema.ts
@@ -1,20 +1,20 @@
-import { pgTable, timestamp, uniqueIndex, varchar } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
 
 import { createId } from "@/helpers/custom-cuid2";
 
-export const user = pgTable(
-    "users",
+import { workspace } from "./workspace.schema";
+
+export const apiKey = pgTable(
+    "api_keys",
     {
         id: varchar()
             .$defaultFn(() => createId())
             .primaryKey().unique(),
-        email: varchar().notNull().unique(),
-        password: varchar().notNull(),
+        workspaceId: varchar().notNull().references(() => workspace.id),
+        name: varchar().notNull(),
+        value: varchar().notNull().unique(),
         createdAt: timestamp().defaultNow().notNull(),
         updatedAt: timestamp().defaultNow().$onUpdate(() => new Date())
             .notNull(),
     },
-    (table) => ({
-        emailUniqueIdx: uniqueIndex().on(table.email),
-    }),
 );
---
+++ src/database/schemas/workspace-user.schema.ts
@@ -0,0 +1,33 @@
+import {
+    boolean,
+    index,
+    pgTable,
+    uniqueIndex,
+    varchar,
+} from "drizzle-orm/pg-core";
+
+import { createId } from "@/helpers/custom-cuid2";
+
+import { user } from "./user.schema";
+import { workspace } from "./workspace.schema";
+
+export const workspaceUser = pgTable(
+    "workspace_users",
+    {
+        id: varchar()
+            .$defaultFn(() => createId())
+            .primaryKey().unique(),
+        workspaceId: varchar().notNull().references(() => workspace.id),
+        userId: varchar().notNull().references(() => user.id),
+        role: varchar({ enum: ["ADMIN", "MODERATOR", "USER"] }).notNull(),
+        owner: boolean().notNull(),
+    },
+    (table) => ({
+        workspaceUserUniqueIdx: uniqueIndex().on(
+            table.workspaceId,
+            table.userId,
+        ),
+        workspaceIdIdx: index().on(table.workspaceId),
+        userIdIdx: index().on(table.userId),
+    }),
+);
