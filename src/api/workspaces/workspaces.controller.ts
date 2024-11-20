51b56716709e67bbad76a5a3ad8c105cb61da94b
Dr-Greg <grigor.mouradyan@outlook.fr>
2024-11-20 18:39:56 +0100

    feat: get user workspace by id

--- src/api/workspaces/workspaces.controller.ts
+++ src/api/workspaces/workspaces.controller.ts
@@ -3,9 +3,14 @@ import { Elysia, error, t } from "elysia";
 import { models } from "@/database/models";
 import { authMiddleware } from "@/hooks/auth.handler";
 
-import { createWorkspace, getUserWorkspaces } from "./workspaces.service";
+import {
+    createWorkspace,
+    getUserWorkspaceById,
+    getUserWorkspaces,
+} from "./workspaces.service";
 
 const { workspace } = models.workspace.insert;
+const { workspace: workspaceSelect } = models.workspace.select;
 
 export default new Elysia().group("/workspaces", (app) => {
     app
@@ -39,6 +44,23 @@ export default new Elysia().group("/workspaces", (app) => {
                 console.error(err);
                 return error(404);
             }
+        })
+        .get("/:workspaceId", async ({ user, params: { workspaceId } }) => {
+            try {
+                const workspaceFromDb = await getUserWorkspaceById(
+                    user.id,
+                    workspaceId,
+                );
+
+                return workspaceFromDb;
+            } catch (err) {
+                console.error(err);
+                return error(404);
+            }
+        }, {
+            params: t.Object({
+                workspaceId: workspaceSelect.id,
+            }),
         });
     return app;
 });
