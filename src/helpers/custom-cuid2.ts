cd4fff55669c1281c4ff3e781eb09273063fcb11
Dr-Greg <grigor.mouradyan@outlook.fr>
2024-11-20 13:26:33 +0100

    feat: add length to createId func

--- src/helpers/custom-cuid2.ts
+++ src/helpers/custom-cuid2.ts
@@ -1,9 +1,5 @@
 import { init, isCuid } from "@paralleldrive/cuid2";
 
-export const createId = init(
-    {
-        length: 8,
-    },
-);
+export const createId = (length: number = 8) => init({ length })();
 
 export const isValidId = isCuid;
