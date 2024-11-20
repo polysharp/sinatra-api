31af209536399dce74495ac2fc2c2b8dfc6741d4
Dr-Greg <grigor.mouradyan@outlook.fr>
2024-11-20 13:58:12 +0100

    feat: add domains route

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,14 @@
 import { Elysia } from "elysia";
 
 import authController from "./api/auth/auth.controller";
-import workspaceController from "./api/workspaces/workspaces.controller";
+import domainController from "./api/domains/domain.controller";
 import sitesController from "./api/sites/site.controller";
 import userController from "./api/users/user.controller";
+import workspaceController from "./api/workspaces/workspaces.controller";
 
 export default new Elysia()
     .use(authController)
+    .use(domainController)
     .use(userController)
-    .use(workspaceController)
-    .use(sitesController);
+    .use(sitesController)
+    .use(workspaceController);
