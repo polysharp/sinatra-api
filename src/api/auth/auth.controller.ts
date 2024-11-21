b566bdb28888f12f554ec0b1b354c7bef6a9ea84
Dr-Greg <grigor.mouradyan@outlook.fr>
2024-11-21 11:52:28 +0100

    feat: add auth cookie to req

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -8,19 +8,37 @@ import { signInService, signUpService } from "./auth.service";
 const { user } = models.user.insert;
 
 export default new Elysia().group("/auth", (app) => {
-    app.post("/sign", async ({ body, set }) => {
+    app.post("/sign", async ({ body, set, cookie: { session } }) => {
         const { email, password } = body;
+
+        let user, token;
 
         const userFromDb = await getUserWithEmail(email, true);
         if (userFromDb) {
-            const { user, token } = await signInService(userFromDb, password);
+            const signInRes = await signInService(userFromDb, password);
+
+            user = signInRes.user;
+            token = signInRes.token;
+
             set.status = 200;
-            return { user, token };
+        } else {
+            const signUpRes = await signUpService(email, password);
+
+            user = signUpRes.user;
+            token = signUpRes.token;
+
+            set.status = 201;
         }
 
-        const { user, token } = await signUpService(email, password);
+        session.set({
+            value: token,
+            path: "/",
+            secure: true,
+            httpOnly: true,
+            sameSite: "strict",
+            maxAge: 60 * 60 * 24 * 30,
+        });
 
-        set.status = 201;
         return { user, token };
     }, {
         body: t.Object({
