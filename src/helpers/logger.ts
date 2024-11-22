7654d149610fdb5f86779949326730f7c4de1f04
Dr-Greg <grigor.mouradyan@outlook.fr>
2024-11-22 17:59:20 +0100

    feat: logger

---
+++ src/helpers/logger.ts
@@ -0,0 +1,72 @@
+import fs from "fs";
+import pino from "pino";
+
+import config from "@/config";
+
+const REDACT_PATHS = [
+  "email",
+  "*.email",
+  "[*].email",
+  "[*].*.email",
+
+  "password",
+  "*.password",
+  "[*].password",
+  "[*].*.password",
+];
+
+class Logger {
+  private readonly logger: pino.Logger;
+
+  constructor(nodeEnv: typeof config.NODE_ENV) {
+    if (!fs.existsSync("./logs")) {
+      fs.mkdirSync("./logs");
+    }
+
+    const streams = [
+      {
+        level: "info",
+        stream: process.stdout,
+      },
+      {
+        level: "info",
+        stream: fs.createWriteStream("./logs/all.log", { flags: "a" }),
+      },
+      {
+        level: "error",
+        stream: fs.createWriteStream("./logs/error.log", { flags: "a" }),
+      },
+    ];
+
+    this.logger = pino(
+      {
+        level: nodeEnv === "test" ? "silent" : "info",
+        timestamp: pino.stdTimeFunctions.isoTime,
+        nestedKey: "payload",
+        redact: {
+          paths: nodeEnv === "production" ? REDACT_PATHS : [],
+          remove: true,
+        },
+      },
+      pino.multistream(streams),
+    );
+  }
+
+  public info(message: string, payload: Record<string, any> = {}): void {
+    this.logger.info(payload, message);
+  }
+
+  public warn(message: string, payload: Record<string, any> = {}): void {
+    this.logger.warn(payload, message);
+  }
+
+  public error(message: string, payload: Record<string, any> = {}): void {
+    this.logger.error(payload, message);
+  }
+
+  public fatal(message: string, payload: Record<string, any> = {}): void {
+    this.logger.fatal(payload, message);
+  }
+}
+
+export default new Logger(config.NODE_ENV);
